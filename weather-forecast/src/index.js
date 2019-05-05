18f304786fe47b36e029df57f2972f1591df415c
Matija Turčec <turcec.matija@gmail.com>
2019-05-05 17:49:41 +0200

    Update index.js

--- weather-forecast/src/index.js
+++ weather-forecast/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App'
 import './main.css';
-//C &#8451; F &#8457;
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
